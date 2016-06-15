9da8790c8b146845ad121c52957e0dd6340becdd
CLEMENT <CLEMENT@10.133.128.147>
2016-06-15 15:18:37 +0200

    cretion of the bddId in motion less class

--- model/src/main/java/motionless/MotionLess.java
+++ model/src/main/java/motionless/MotionLess.java
@@ -7,8 +7,14 @@ import element.Sprite;
 
 public abstract class MotionLess extends Element {
 	
-	public MotionLess(final Sprite sprite, final Permeability permeability){
+	private final int bddId;
+	
+	public MotionLess(final Sprite sprite, final Permeability permeability, final int bddId){
 		super(sprite,permeability);
+		this.bddId = bddId;
 	}
 
+	public int getBddId() {
+		return bddId;
+	}
 }
