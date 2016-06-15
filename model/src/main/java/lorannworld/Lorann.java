fe76b5897fe5fb808783cec509669b77996a4842
QuentinFavresse <quentin.favresse@viacesi.fr>
2016-06-15 11:12:40 +0200

    do Motionless and element (àfaire)

--- model/src/main/java/lorannworld/Lorann.java
+++ model/src/main/java/lorannworld/Lorann.java
@@ -1,9 +1,23 @@
 package lorannworld;
 
 public class Lorann {
-	
+	@SuppressWarnings("unused")
+	private Integer ZOOM = 4;
 	public Lorann(){
 		
 	}
-
+	
+	public void play() {
+	}
+	
+	public LorannPlay getLorannPlay() {
+		return null;
+	}
+	
+	public LorannWorld getLorannWorld() {
+		return null;
+	} 
+	
+	public void run() {
+	}
 }
--- model/src/main/java/lorannworld/LorannWorld.java
+++ model/src/main/java/lorannworld/LorannWorld.java
@@ -1,9 +1,85 @@
 package lorannworld;
 
+import java.util.ArrayList;
+
+import element.Element;
+import mobile.Hero;
+import mobile.Mobile;
+
 public class LorannWorld {
+	private Integer width;
+	private Integer height;
+	
+	public Integer getWidth() {
+		return width;
+	}
+
+	public Integer getHeight() {
+		return height;
+	}
+
+
 	
 	public LorannWorld(){
 		
 	}
+	
+	public LorannWorld(Integer width, Integer height){
+		this.width = width;
+		this.height = height;
+	}
 
+
+	public LorannWorld(String fileName){
+		
+	}
+	
+	public void addElement(Element element, Integer x, Integer y){
+		
+	}
+	
+	public void getElement(Integer x, Integer y){
+		
+	}
+	
+	public void addMobile(Hero hero, Integer x, Integer y){
+		
+	}
+
+	public Element[][] getElement(){
+		return null;
+		
+	}
+	
+	public Hero getHero(){
+		return null;
+		
+	}
+	
+	public ArrayList<Mobile> getMobiles(){
+		return null;
+		
+	}
+
+	public void setHero(Hero hero){
+		
+	}
+	
+	public void setMobilehasChanged(){
+		
+	}
+	
+	public void notifyObservers(){
+		
+	}
+	
+	public void addMobile(Mobile mobile, Integer x, Integer y){
+		
+	}
+	
+	@SuppressWarnings("unused")
+	private void loadLorannWorld(){
+		
+	}
 }
+
