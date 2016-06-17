c89e97dc3d447e13ff3ed078919c8125b3b744de
QuentinFavresse <quentin.favresse@viacesi.fr>
2016-06-17 16:38:46 +0200

    Add/remove some tests

--- controller/src/test/controller/controller/ControllerTest.java
+++ controller/src/test/controller/controller/ControllerTest.java
@@ -2,12 +2,27 @@ package controller;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class ControllerTest {
 
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
 	@Test
-	public void test() {
+	public void testControl() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testOrderPerform() {
 		fail("Not yet implemented");
 	}
 
