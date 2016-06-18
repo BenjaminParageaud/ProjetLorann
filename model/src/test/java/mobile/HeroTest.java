0c919b288c9c9c10d6eec3a10e390580f2cfd3c3
QuentinFavresse <quentin.favresse@viacesi.fr>
2016-06-18 17:09:53 +0200

    MobileTest&MobileLessTest OK

--- model/src/test/java/mobile/HeroTest.java
+++ model/src/test/java/mobile/HeroTest.java
@@ -2,12 +2,67 @@ package mobile;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class HeroTest {
 
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
 	@Test
-	public void test() {
+	public void testGetLorannWorldEntity() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMoveUp() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMoveDown() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMoveRight() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMoveLeft() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetLorannWorldEntityILorannWorldEntityIntInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetX() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetX() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetY() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetY() {
 		fail("Not yet implemented");
 	}
 
