9706385d7cf2fbbdb0b63ed055fc860172a89433
QuentinFavresse <quentin.favresse@viacesi.fr>
2016-06-19 15:59:29 +0200

    Update of some test, testDBConnection= OK

--- model/src/test/java/mobile/MobileTest.java
+++ model/src/test/java/mobile/MobileTest.java
@@ -2,13 +2,25 @@ package mobile;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class MobileTest {
+	private static Hero lorannTest;
+	private static int bddIdOfLorannTest = 1;
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		lorannTest = new Hero(null);
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testGetBddId() {
+		assertEquals(bddIdOfLorannTest, MobileTest.lorannTest.getBddId());
 	}
 
 }
--- model/src/test/java/model/DBConnectionTest.java
+++ model/src/test/java/model/DBConnectionTest.java
@@ -2,13 +2,28 @@ package model;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class DBConnectionTest {
+	private static DBConnection testDBConnection;
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
 
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/*@Test
+	public void testGetInstance() {
+	}*/
+
+	@SuppressWarnings("static-access")
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testGetConnection() {
+		assertNotNull(testDBConnection.getInstance());
 	}
 
 }
