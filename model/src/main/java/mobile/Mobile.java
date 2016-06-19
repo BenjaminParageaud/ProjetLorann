74613cdd87f0c9fb6018b14f671c540e28b8dc7f
CLEMENT <CLEMENT@Ordi-de-PopClem>
2016-06-19 05:56:10 +0200

    commit for quentin

--- model/src/main/java/mobile/Mobile.java
+++ model/src/main/java/mobile/Mobile.java
@@ -43,7 +43,7 @@ public abstract class Mobile extends Element implements IMobile{
 	
 	public void setX(int x) {
 		this.position.x = x;
-		//this.getLorannWorldEntity().setMobilehasChanged();
+		this.getLorannWorldEntity().setMobilehasChanged();
 	}
 
 	public int getY() {
@@ -52,7 +52,7 @@ public abstract class Mobile extends Element implements IMobile{
 
 	public void setY(int y) {
 		this.position.y = y;
-		//this.getLorannWorldEntity().setMobilehasChanged();
+		this.getLorannWorldEntity().setMobilehasChanged();
 	}
 	
 	public ISprite getSprite() {
@@ -98,6 +98,7 @@ public abstract class Mobile extends Element implements IMobile{
 
 	public void setLorannWorldEntity(final ILorannWorldEntity lorannWorldEntity, final int x, final int y) {
 		super.setLorannWorldEntity(lorannWorldEntity);;
+		this.lorannWorldEntity = lorannWorldEntity;
 		this.setX(x);
 		this.setY(y);
 	}
