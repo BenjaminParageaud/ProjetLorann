9c2ca5f252b97576e0b9adecb21073a1226907ba
BenjaminParageaud <benjitoon33@gmail.com>
2016-06-20 10:48:25 +0200

    add in Spell class

--- model/src/main/java/mobile/Spell.java
+++ model/src/main/java/mobile/Spell.java
@@ -3,22 +3,46 @@ package mobile;
 import contract.ILorannWorldEntity;
 import contract.IMobile;
 import contract.ISpell;
+import contract.ISprite;
 import contract.Permeability;
 import element.Sprite;
+import motionless.MotionLessFactory;
 
 public class Spell extends Mobile implements ISpell{
+	public int coordonate;
+	private ISprite sprite; 
+	private Permeability permeability;
 	
 	public Spell(ILorannWorldEntity lorannWorldEntity){
 		super(new Sprite("SpellAnim.gif"), Permeability.SPELLABLE, 1, lorannWorldEntity);
 	}
 
 	public ISpell AffichageSpell(final int x, final int y){
+	   
 			return null;
+	}
+	
+	public ILorannWorldEntity getLorannWorldEntity() {
+		return lorannWorldEntity;
+	}
+	
+	public void setSprite(ISprite sprite) {
+		this.sprite = sprite;
+	}
+	
+	public boolean isMovePossible(final int x, final int y){
+		if(getLorannWorldEntity().getElement(x, y) == null){
+			return(MobileFactory.getFromBddIdHero(1, lorannWorldEntity).getPermeability() == (Permeability.SPELLABLE)); 
+		}
+		else {
+			coordonate = getLorannWorldEntity().getElement(x, y).getBddId();
+			return(MotionLessFactory.getFromBddId(coordonate).getPermeability() == (Permeability.MEETINGABLE)|| MotionLessFactory.getFromBddId(coordonate).getPermeability() == (Permeability.PENETRABLE )); 
+		}
 	}
 
 	public Boolean executeMoveIfPossible(final int x, final int y){
 		boolean a = false;
-		if (this.getLorannWorldEntity().getElement(x, y)==null)
+		if (this.getLorannWorldEntity().getElement(x, y)== null)
 		{
 			a = true;
 		} else {
@@ -26,6 +50,16 @@ public class Spell extends Mobile implements ISpell{
 		}
 		return a;
 	}
+	
+	public void setLorannWorldEntity(ILorannWorldEntity lorannWorldEntity) {
+		this.lorannWorldEntity = lorannWorldEntity;
+	}
+
+
+	public void setPermeability(Permeability permeability) {
+		this.permeability = permeability;
+	}
+	
 /*
 		for(final IMobile I : this.getLorannWorldEntity().getMotionElements())
 		{
